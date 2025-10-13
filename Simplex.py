ddd1c0f35bbad3fa6ca3bc3f0504a528c21c33f1
Andrew Hockman <spamdrew128@gmail.com>
2025-10-13 17:36:46 -0400

    update changing var

--- Simplex.py
+++ Simplex.py
@@ -90,7 +90,7 @@ def graph_params_vs_n_or_m(df, const_val, const_var='m', filepath="outputs\\plot
     fig, ax1 = plt.subplots()
     title = "Pivot Count and Runtime with {}={} vs {}".format(const_var, const_val, changing_var)
     plt.title(title)
-    ax1.set_xlabel('n')
+    ax1.set_xlabel(changing_var)
     
     pivot_color = "red"
     ax1.set_ylabel('Pivot Count', color=pivot_color)
